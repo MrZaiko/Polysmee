85ea350b30dd19c7ee3bcf560912163b654c7c18
ledupont <leo.dupont@epfl.ch>
2021-03-09 18:21:46 +0100

    Appointment creation tests finished

---
+++ app/src/androidTest/java/io/github/polysmee/AppointmentActivityTest.java
@@ -0,0 +1,124 @@
+package io.github.polysmee;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import io.github.polysmee.appointments.AppointmentActivity;
+import io.github.polysmee.appointments.TestUser;
+import io.github.polysmee.interfaces.Appointment;
+
+import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
+import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed;
+import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
+import static com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo;
+import static com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setDateOnPicker;
+import static com.schibsted.spain.barista.interaction.BaristaPickerInteractions.setTimeOnPicker;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class AppointmentActivityTest {
+    private static final TestUser TEST_USER = new TestUser("username");
+    String title = "title";
+    String course = "course";
+    String startTime = "23/03/2021   -   17:02";
+    String endTime = "23/03/2021   -   18:02";
+
+    @Rule
+    public ActivityScenarioRule<AppointmentActivity> testRule = new ActivityScenarioRule<>(AppointmentActivity.class);
+
+    @Test
+    public void btnCreateCreatesCorrectAppointment() {
+        clickOn(R.id.btnStartTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(17, 2);
+
+        clickOn(R.id.btnEndTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(18, 2);
+
+        writeTo(R.id.editTxtAppointmentTitleSet, title);
+
+        writeTo(R.id.editTxtAppointmentCourseSet, course);
+
+        clickOn(R.id.btnCreateAppointment);
+
+        Appointment appointment = (Appointment) testRule.getScenario().getResult().getResultData().getSerializableExtra(AppointmentActivity.EXTRA_APPOINTMENT);
+        assertEquals(appointment.getTitle(), title);
+        assertEquals(appointment.getCourse(), course);
+        Calendar startCalendar = new GregorianCalendar();
+        startCalendar.set(2021, 3, 23, 17, 2, 0);
+        startCalendar.set(Calendar.MILLISECOND, 0);
+        Calendar endCalendar = new GregorianCalendar();
+        endCalendar.set(2021, 3, 23, 18, 2, 0);
+        endCalendar.set(Calendar.MILLISECOND, 0);
+        assertEquals(appointment.getStartTime(), startCalendar.getTimeInMillis());
+        assertEquals(appointment.getDuration(), endCalendar.getTimeInMillis() - startCalendar.getTimeInMillis());
+    }
+
+    @Test
+    public void btnCreateSaysErrorHappenedOnIncorrectStartAndEndTime() {
+        clickOn(R.id.btnStartTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(17, 2);
+
+        clickOn(R.id.btnEndTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(16, 2);
+
+        writeTo(R.id.editTxtAppointmentTitleSet, title);
+
+        writeTo(R.id.editTxtAppointmentCourseSet, course);
+
+        clickOn(R.id.btnCreateAppointment);
+        assertDisplayed(R.id.txtError, AppointmentActivity.ERROR_TXT);
+    }
+
+    @Test
+    public void btnStartTimeGetsTime() {
+        String startTime = "23/03/2021   -   17:02";
+        clickOn(R.id.btnStartTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(17, 2);
+        assertDisplayed(R.id.txtStartTime, startTime);
+        assertNotDisplayed(R.id.txtError);
+    }
+
+    @Test
+    public void btnEndTimeGetsTime(){
+        String endTime = "23/03/2021   -   18:02";
+        clickOn(R.id.btnEndTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(18, 2);
+        assertDisplayed(R.id.txtEndTime, endTime);
+        assertNotDisplayed(R.id.txtError);
+    }
+
+    @Test
+    public void btnResetResets() {
+        clickOn(R.id.btnStartTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(17, 2);
+
+        clickOn(R.id.btnEndTime);
+        setDateOnPicker(2021, 3, 23);
+        setTimeOnPicker(18, 2);
+
+        writeTo(R.id.editTxtAppointmentCourseSet, course);
+
+        writeTo(R.id.editTxtAppointmentTitleSet, title);
+
+        clickOn(R.id.btnReset);
+        assertDisplayed(R.id.txtStartTime, "Start Time");
+        assertNotDisplayed(R.id.txtError);
+        assertDisplayed(R.id.txtEndTime, "End Time");
+        assertDisplayed(R.id.editTxtAppointmentCourseSet, "Appointment Course");
+        assertDisplayed(R.id.editTxtAppointmentTitleSet, "Appointment Title");
+    }
+}
--- app/src/main/java/io/github/polysmee/appointments/AppointmentActivity.java
+++ app/src/main/java/io/github/polysmee/appointments/AppointmentActivity.java
@@ -1,52 +1,60 @@
 package io.github.polysmee.appointments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
 import io.github.polysmee.R;
-import io.github.polysmee.interfaces.Appointment;
 import io.github.polysmee.interfaces.User;
 
 public class AppointmentActivity extends AppCompatActivity {
-    private static final String TAG = "AppointmentActivity";
+    public static final String EXTRA_USER = "user";
+    public static final String EXTRA_APPOINTMENT = "appointment";
+    public static final String ERROR_TXT = "Error : Start and end time must result in a correct time slot";
     EditText editTitle, editCourse;
     Button btnStartTime, btnEndTime, btnCreate, btnReset;
     Calendar clndrStartTime, clndrEndTime;
-    TextView txtError;
+    TextView txtError, txtStartTime, txtEndTime;
+
     User user;
 
     Calendar date;
-    public void showDateTimePicker() {
+    public void showDateTimePicker(boolean isStart) {
         final Calendar currentDate = Calendar.getInstance();
         date = Calendar.getInstance();
-        new DatePickerDialog(AppointmentActivity.this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                date.set(year, monthOfYear, dayOfMonth);
-                new TimePickerDialog(AppointmentActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        date.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                        date.set(Calendar.MINUTE, minute);
-                        Log.v(TAG, "The chosen one " + date.getTime());
-                    }
-                }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), true).show();
-            }
+        new DatePickerDialog(AppointmentActivity.this, (view, year, monthOfYear, dayOfMonth) -> {
+            date.set(year, monthOfYear + 1, dayOfMonth);
+            new TimePickerDialog(AppointmentActivity.this, (view1, hourOfDay, minute) -> {
+                date.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                date.set(Calendar.MINUTE, minute);
+                date.set(Calendar.SECOND, 0);
+                date.set(Calendar.MILLISECOND, 0);
+                updateCalendar(isStart);
+            }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), true).show();
         }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
+    }
+
+    private void updateCalendar(boolean isStart) {
+        txtError.setVisibility(View.INVISIBLE);
+        if(isStart) {
+            clndrStartTime = (Calendar) date.clone();
+            String strStartTime = clndrStartTime.get(Calendar.DAY_OF_MONTH) + "/" + String.format("%02d", clndrStartTime.get(Calendar.MONTH)) + "/" + clndrStartTime.get(Calendar.YEAR) + "   -   " + String.format("%02d", clndrStartTime.get(Calendar.HOUR_OF_DAY)) + ":" + String.format("%02d", clndrStartTime.get(Calendar.MINUTE));
+            txtStartTime.setText(strStartTime);
+        } else {
+            clndrEndTime = (Calendar) date.clone();
+            String strEndTime = clndrEndTime.get(Calendar.DAY_OF_MONTH) + "/" + String.format("%02d", clndrEndTime.get(Calendar.MONTH)) + "/" + clndrEndTime.get(Calendar.YEAR) + "   -   " + String.format("%02d", clndrEndTime.get(Calendar.HOUR_OF_DAY)) + ":" + String.format("%02d", clndrEndTime.get(Calendar.MINUTE));
+            txtEndTime.setText(strEndTime);
+        }
     }
 
     @Override
@@ -54,46 +62,52 @@ public class AppointmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
 
-        editTitle = (EditText) findViewById(R.id.editTxtAppointmentTitleSet);
-        editCourse = (EditText) findViewById(R.id.editTxtAppointmentCourseSet);
-        btnStartTime = (Button) findViewById(R.id.btnStartTime);
-        btnEndTime = (Button) findViewById(R.id.btnEndTime);
-        btnCreate = (Button) findViewById(R.id.btnCreateAppointment);
-        btnReset = (Button) findViewById(R.id.btnReset);
-        txtError = (TextView) findViewById(R.id.txtError);
+        editTitle = findViewById(R.id.editTxtAppointmentTitleSet);
+        editCourse = findViewById(R.id.editTxtAppointmentCourseSet);
+        btnStartTime = findViewById(R.id.btnStartTime);
+        btnEndTime = findViewById(R.id.btnEndTime);
+        btnCreate = findViewById(R.id.btnCreateAppointment);
+        btnReset = findViewById(R.id.btnReset);
+        txtError = findViewById(R.id.txtError);
+        txtStartTime = findViewById(R.id.txtStartTime);
+        txtEndTime = findViewById(R.id.txtEndTime);
+
+        txtError.setVisibility(View.INVISIBLE);
 
         //We need to know who is trying to create an appointment as they are the owner, see BasicAppointment implementation
-        user = (User) getIntent().getSerializableExtra("user");
+        user = (User) getIntent().getSerializableExtra(EXTRA_USER);
 
         btnStartTime.setOnClickListener(v -> {
-            showDateTimePicker();
-            clndrStartTime = (Calendar) date.clone();
+            showDateTimePicker(true);
         });
 
         btnEndTime.setOnClickListener(v -> {
-            showDateTimePicker();
-            clndrEndTime = (Calendar) date.clone();
+            showDateTimePicker(false);
         });
 
         btnCreate.setOnClickListener(v -> {
             if(clndrStartTime.getTimeInMillis() >= clndrEndTime.getTimeInMillis() || clndrStartTime.getTimeInMillis() <= Calendar.getInstance().getTimeInMillis()) {
-                txtError.setText("Error : Start and end time must result in a correct time slot");
+                txtError.setVisibility(View.VISIBLE);
+                txtError.setText(ERROR_TXT);
             } else {
                 String title = editTitle.getText().toString();
                 String course = editCourse.getText().toString();
-                BasicAppointment appointment = new BasicAppointment(clndrStartTime.getTimeInMillis(), clndrEndTime.getTimeInMillis(), course, title, user);
+                BasicAppointment appointment = new BasicAppointment(clndrStartTime.getTimeInMillis(), clndrEndTime.getTimeInMillis() - clndrStartTime.getTimeInMillis(), course, title, user);
                 Intent returnIntent = new Intent();
-                returnIntent.putExtra("appointment", appointment);
+                returnIntent.putExtra(EXTRA_APPOINTMENT, appointment);
                 setResult(Activity.RESULT_OK, returnIntent);
                 finish();
             }
         });
 
         btnReset.setOnClickListener(v -> {
-            editTitle.setText("");
-            editCourse.setText("");
+            editTitle.setText("Appointment Title");
+            editCourse.setText("Appointment Course");
+            txtError.setText("");
             clndrEndTime = Calendar.getInstance();
+            txtEndTime.setText("End Time");
             clndrStartTime = Calendar.getInstance();
+            txtStartTime.setText("Start Time");
         });
     }
 }
