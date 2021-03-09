fd7fdd296cd1a79385e8144e0832bc280dc4bde6
MrZaik0 <rabidooze@gmail.com>
2021-03-09 22:35:52 +0100

    Fix tests by adding sleep

--- app/src/androidTest/java/io/github/polysmee/roomActivityTests/roomActivityMessagesFragmentTest.java
+++ app/src/androidTest/java/io/github/polysmee/roomActivityTests/roomActivityMessagesFragmentTest.java
@@ -12,6 +12,7 @@ import io.github.polysmee.room.fragments.roomActivityMessagesFragment;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertContains;
@@ -19,6 +20,7 @@ import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.
 import static com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertNotContains;
 import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
 import static com.schibsted.spain.barista.interaction.BaristaEditTextInteractions.writeTo;
+import static com.schibsted.spain.barista.internal.viewaction.SleepViewAction.sleep;
 
 @RunWith(JUnit4.class)
 public class roomActivityMessagesFragmentTest {
@@ -27,6 +29,7 @@ public class roomActivityMessagesFragmentTest {
         FragmentScenario.launchInContainer(roomActivityMessagesFragment.class);
         String message = "A message";
         onView(withId(R.id.roomActivityMessageText)).perform(typeText(message), closeSoftKeyboard());
+        sleep(1000);
         assertDisplayed(R.id.roomActivityMessageText, message);
         onView(withId(R.id.roomActivitySendMessageButton)).perform(click());
         assertContains(R.id.roomActivityMessageText, "");
@@ -37,16 +40,19 @@ public class roomActivityMessagesFragmentTest {
         FragmentScenario.launchInContainer(roomActivityMessagesFragment.class);
         String message = "A message";
         onView(withId(R.id.roomActivityMessageText)).perform(typeText(message), closeSoftKeyboard());
+        sleep(1000);
         onView(withId(R.id.roomActivitySendMessageButton)).perform(click());
         assertNotContains(R.id.roomActivityMessageText, message);
         assertDisplayed(message);
     }
+
 
     @Test
     public void receiveButtonShouldClearMessageText() {
         FragmentScenario.launchInContainer(roomActivityMessagesFragment.class);
         String message = "A message";
         onView(withId(R.id.roomActivityMessageText)).perform(typeText(message), closeSoftKeyboard());
+        sleep(1000);
         onView(withId(R.id.roomActivityReceiveMessageButton)).perform(click());
         assertContains(R.id.roomActivityMessageText, "");
     }
@@ -56,6 +62,7 @@ public class roomActivityMessagesFragmentTest {
         FragmentScenario.launchInContainer(roomActivityMessagesFragment.class);
         String message = "A message";
         onView(withId(R.id.roomActivityMessageText)).perform(typeText(message), closeSoftKeyboard());
+        sleep(1000);
         assertDisplayed(R.id.roomActivityMessageText, message);
         onView(withId(R.id.roomActivityReceiveMessageButton)).perform(click());
         assertNotContains(R.id.roomActivityMessageText, message);
