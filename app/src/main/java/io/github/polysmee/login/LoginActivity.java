4f243b61515c8941b9ea3f50123f6d60308ad218
Mathis Randl <mathis.randl@epfl.ch>
2021-03-09 09:28:52 +0100

    added copyright license for firebase

--- app/src/main/java/io/github/polysmee/login/LoginActivity.java
+++ app/src/main/java/io/github/polysmee/login/LoginActivity.java
@@ -17,6 +17,20 @@ import java.util.List;
 import io.github.polysmee.MainActivity;
 import io.github.polysmee.R;
 
+//Copyright 2017 github.com/firebase
+
+//        Licensed under the Apache License, Version 2.0 (the "License");
+//        you may not use this file except in compliance with the License.
+//        You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//        Unless required by applicable law or agreed to in writing, software
+//        distributed under the License is distributed on an "AS IS" BASIS,
+//        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//        See the License for the specific language governing permissions and
+//        limitations under the License.
+
 //SOURCE (copied from there and then reworked) : https://github.com/firebase/snippets-android/blob/05f35b44cee182242cea761d68dde58c8a0af3d6/auth/app/src/main/java/com/google/firebase/quickstart/auth/FirebaseUIActivity.java#L31-L45
 
 public class LoginActivity extends AppCompatActivity {
