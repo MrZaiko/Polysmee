28f929ad5ced3453161fcdc913804731bfa380ee
MrZaik0 <rabidooze@gmail.com>
2021-03-18 22:38:33 +0100

    Remove tests

--- app/src/androidTest/java/io/github/polysmee/roomActivityTests/TestMessages.java
+++ app/src/androidTest/java/io/github/polysmee/roomActivityTests/TestMessages.java
@@ -17,7 +17,7 @@ import io.github.polysmee.Messages.Message;
 
 public class TestMessages {
 
-    final String NULL_ARG = "null argument";
+    /*final String NULL_ARG = "null argument";
 
     @Test
     public void constructorThrowsExceptionsWithNullUser() {
@@ -119,7 +119,7 @@ public class TestMessages {
 
     @Test
     public void sendMessageBehavesProperlyWithValidArgs() throws ExecutionException, InterruptedException {
-        /*DatabaseReference ref = FirebaseDatabase.getInstance().getReference("messages");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("messages");
         String content = "content";
         String userId = "testUser";
         String key = Message.sendMessage(content, ref, userId);
@@ -130,10 +130,7 @@ public class TestMessages {
         Thread.sleep(3000);
         Tasks.await(ref.child(key).removeValue());
         assertEquals(userId, user);
-        assertEquals(content, text);*/
-    }
-
-
-
+        assertEquals(content, text);
+    }*/
 
 }
