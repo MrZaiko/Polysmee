cba03769fa0fb2ba310eb27e1a594260de7a22f0
Mathis Randl <mathis.randl@epfl.ch>
2021-03-30 18:02:00 +0200

    working on getting banned users to work

--- app/src/main/java/io/github/polysmee/interfaces/User.java
+++ app/src/main/java/io/github/polysmee/interfaces/User.java
@@ -17,28 +17,7 @@ public interface User{
      */
     String getId();
 
-    /**
-     * Retrieves the user's name
-     * @return user's name
-     */
-    @Deprecated
-    String getName();
-
     default void getNameAndThen(StringValueListener valueListener) {}
-
-    /**
-     * Retrieves the user's surname
-     * @return user's surname
-     */
-    @Deprecated
-    String getSurname();
-
-    /**
-     * Retrieves the set of the upcoming appointments for this user
-     * @return user's set of appointment in an unmodifiable set
-     */
-    @Deprecated
-    Set<Appointment> getAppointments();
 
     default void getAppointmentsAndThen(StringSetValueListener valueListener) {}
 
