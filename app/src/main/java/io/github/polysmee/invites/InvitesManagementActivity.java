eed1d145c8b6c24fb5e7b83b235c0fb49b1a7aef
ledupont <leo.dupont@epfl.ch>
2021-04-20 11:38:27 +0200

    Created necessary layouts

---
+++ app/src/main/java/io/github/polysmee/invites/InvitesManagementActivity.java
@@ -0,0 +1,16 @@
+package io.github.polysmee.invites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.polysmee.R;
+
+public class InvitesManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invites_management);
+    }
+}
