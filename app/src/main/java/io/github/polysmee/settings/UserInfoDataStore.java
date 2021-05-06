2dd873a7f262edd932d852e9a84e18f30b31dae8
tombenamato <tombenamato@gmail.com>
2021-05-06 16:54:35 +0200

    change what the linter suggest

--- app/src/main/java/io/github/polysmee/settings/UserInfoDataStore.java
+++ app/src/main/java/io/github/polysmee/settings/UserInfoDataStore.java
@@ -12,34 +12,25 @@ import io.github.polysmee.login.MainUserSingleton;
  * more precisely only the value corresponding to the main user name in the application
  */
 public final class UserInfoDataStore extends PreferenceDataStore {
-    private User dataBaseMainUser = MainUserSingleton.getInstance();
+    private final User dataBaseMainUser = MainUserSingleton.getInstance();
     public static final String preferenceKeyMainUserName = "preference_key_main_user_info_name";
-    public static String preferenceKeyMainUserEmail = "preference_key_main_user_info_email";
+    public static final String preferenceKeyMainUserEmail = "preference_key_main_user_info_email";
 
 
     @Override
-    /**
-     * It only save one map, this map have the key for the main user name. It will save this value on the database
-     */
+    //It only save one map, this map have the key for the main user name. It will save this value on the database
     public void putString(@NonNull String key, @Nullable String value) {
         if(value==null){
             return;
         }
-        switch (key){
-            case preferenceKeyMainUserName :
-                dataBaseMainUser.setName(value);
-                break;
-            default:
-                return;
+        if(key.equals(preferenceKeyMainUserName)){
+            dataBaseMainUser.setName(value);
         }
     }
 
 
     @Override
-    @Nullable
-    /**
-     * never return a value
-     */
+    //always return empty string
     public String getString(@NonNull String key, @Nullable String defValue) {
         return "";
     }
