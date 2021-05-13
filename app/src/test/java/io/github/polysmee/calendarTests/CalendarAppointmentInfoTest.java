30d9c9054cc8e94626d8b663e9c25cb2092f4295
tombenamato <tombenamato@gmail.com>
2021-05-13 18:15:42 +0200

    optimize import and reformat

--- app/src/test/java/io/github/polysmee/calendarTests/CalendarAppointmentInfoTest.java
+++ app/src/test/java/io/github/polysmee/calendarTests/CalendarAppointmentInfoTest.java
@@ -5,25 +5,19 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import io.github.polysmee.calendar.CalendarAppointmentInfo;
-import io.github.polysmee.database.User;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
 public class CalendarAppointmentInfoTest {
 
     @Test
-    public void appointmentInfoGettersWorkCorrectly(){
-        CalendarAppointmentInfo calendarAppointmentInfo = new CalendarAppointmentInfo("Course","Title",
-                0,0,"0");
-        assertEquals("Course",calendarAppointmentInfo.getCourse());
-        assertEquals("Title",calendarAppointmentInfo.getTitle());
-        assertEquals(0,calendarAppointmentInfo.getStartTime());
-        assertEquals(0,calendarAppointmentInfo.getDuration());
-        assertEquals("0",calendarAppointmentInfo.getId());
+    public void appointmentInfoGettersWorkCorrectly() {
+        CalendarAppointmentInfo calendarAppointmentInfo = new CalendarAppointmentInfo("Course", "Title",
+                0, 0, "0");
+        assertEquals("Course", calendarAppointmentInfo.getCourse());
+        assertEquals("Title", calendarAppointmentInfo.getTitle());
+        assertEquals(0, calendarAppointmentInfo.getStartTime());
+        assertEquals(0, calendarAppointmentInfo.getDuration());
+        assertEquals("0", calendarAppointmentInfo.getId());
         //didn't put for user because equals is not defined for FakeDatabaseUser
 
     }
