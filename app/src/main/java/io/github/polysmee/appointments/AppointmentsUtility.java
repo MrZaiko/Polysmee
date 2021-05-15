de7a0dea12c758b5d01a4542fd666691ecc781c5
Attia Youssef <youssef.attia@epfl.ch>
2021-05-15 17:45:59 +0100

    remove unused method

--- app/src/main/java/io/github/polysmee/appointments/AppointmentsUtility.java
+++ app/src/main/java/io/github/polysmee/appointments/AppointmentsUtility.java
@@ -13,17 +13,19 @@ import io.github.polysmee.profile.AutoCompleteUserAdapter;
 import io.github.polysmee.profile.UserItemAutocomplete;
 
 public class AppointmentsUtility {
-    public static void usersNamesGetter(Set<String> allIds, ArrayList<String> users) {
-        //This function is called at the creation of the fragment
-        //So here we get the names at the beginning of the fragment's life cycle and the listeners should updated them, but not remove the old name
-        //While this may cause small problems if a user changes their name during this time,
-        //the life cycle is expected to be pretty short and users shouldn't often change their name so it should only very rarely occur.
-        for (String userId : allIds) {
-            User user = new DatabaseUser(userId);
-            user.getName_Once_AndThen(users::add);
-        }
-    }
 
+    /**
+     * This function is called at the creation of the fragment to set a custom adapter for a given autoCompleteTextView
+     * So here we get the names at the beginning of the fragment's life cycle and the listeners should updated them, but not remove the old name
+     * While this may cause small problems if a user changes their name during this time,
+     * the life cycle is expected to be pretty short and users shouldn't often change their name so it should only very rarely occur.
+     * It also fills the all users list with the corresponding UserItemAutocomplete
+     * @param ids set of all users ids
+     * @param allUsersNames list of names to fill
+     * @param allUsers list of UserItemAutoCompleteToFill
+     * @param autoCompleteTextView the autoCompleteTextView for which we will set the custom adapter
+     * @param context context needed to create the custom adapter
+     */
     public static void fillUserList (Set<String> ids, List<String> allUsersNames, List<UserItemAutocomplete> allUsers, AutoCompleteTextView autoCompleteTextView, Context context) {
         for (String id : ids) {
             User user = new DatabaseUser(id);
