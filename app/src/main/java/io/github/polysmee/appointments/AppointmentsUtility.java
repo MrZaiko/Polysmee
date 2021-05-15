2d33a52c5f81e0bd2e331fe0dcaf1ee99dfb4141
Attia Youssef <youssef.attia@epfl.ch>
2021-05-15 16:23:11 +0100

    Change user search when creating appointment

--- app/src/main/java/io/github/polysmee/appointments/AppointmentsUtility.java
+++ app/src/main/java/io/github/polysmee/appointments/AppointmentsUtility.java
@@ -1,10 +1,16 @@
 package io.github.polysmee.appointments;
 
+import android.content.Context;
+import android.widget.AutoCompleteTextView;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import io.github.polysmee.database.DatabaseUser;
 import io.github.polysmee.database.User;
+import io.github.polysmee.profile.AutoCompleteUserAdapter;
+import io.github.polysmee.profile.UserItemAutocomplete;
 
 public class AppointmentsUtility {
     public static void usersNamesGetter(Set<String> allIds, ArrayList<String> users) {
@@ -15,6 +21,26 @@ public class AppointmentsUtility {
         for (String userId : allIds) {
             User user = new DatabaseUser(userId);
             user.getName_Once_AndThen(users::add);
+        }
+    }
+
+    public static void fillUserList (Set<String> ids, List<String> allUsersNames, List<UserItemAutocomplete> allUsers, AutoCompleteTextView autoCompleteTextView, Context context) {
+        for (String id : ids) {
+            User user = new DatabaseUser(id);
+            UserItemAutocomplete userItemAutocomplete = new UserItemAutocomplete();
+            user.getName_Once_AndThen((name) -> {
+                allUsersNames.add(name);
+
+                userItemAutocomplete.setUsername(name);
+                user.getProfilePicture_Once_And_Then((profilePictureId) ->{
+                    userItemAutocomplete.setPictureId(profilePictureId);
+                    allUsers.add(userItemAutocomplete);
+                    if(allUsers.size() == ids.size()){
+                        autoCompleteTextView.setAdapter(new AutoCompleteUserAdapter(context,allUsers));
+                    }
+                });
+            });
+
         }
     }
 }
