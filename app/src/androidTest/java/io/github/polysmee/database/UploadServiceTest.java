f63f7b6c094dd7f2a445618097f46a46d4c87f96
Mathis Randl <mathis.randl@epfl.ch>
2021-05-17 00:08:07 +0200

    added coverage

--- app/src/androidTest/java/io/github/polysmee/database/UploadServiceTest.java
+++ app/src/androidTest/java/io/github/polysmee/database/UploadServiceTest.java
@@ -2,18 +2,20 @@ package io.github.polysmee.database;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class UploadServiceTest {
     @Test
-    public void uploadDownloadDelete() throws InterruptedException {
+    public void uploadDownloadDelete() {
         UploadServiceFactory.setTest(true);
         UploadService us = UploadServiceFactory.getAdaptedInstance();
         us.uploadImage(
@@ -38,24 +40,35 @@ public class UploadServiceTest {
 
         UploadServiceFactory.setTest(false);
         us = UploadServiceFactory.getAdaptedInstance();
+
+        us.downloadImage(
+            "non-existent image",
+            Assert::assertNull,
+            (exc) -> {throw new IllegalStateException("crashed in test");},
+            getApplicationContext()
+        );
+
+        UploadService finalUs = us;
+        UploadService finalUs1 = us;
+
         us.uploadImage(
                 new byte[]{2, 3, 4},
                 "nums",
-                (name) -> assertTrue(name.contains("nums")),
-                (exc) -> {
+                (name) -> finalUs.downloadImage(
+                    name,
+                    (gotten) -> finalUs1.deleteImage(
+                        "nums",
+                        (nam) -> assertEquals(name, nam),
+                        (exc) -> {throw new IllegalStateException("crashed in test 2");
+                        }, getApplicationContext()
+                    ),
+                    (exc) -> {throw new IllegalStateException("crashed in test 3");
+                    }, getApplicationContext()
+                ),
+                (exc) -> {throw new IllegalStateException("crashed in test 4");
                 }, getApplicationContext()
         );
-        us.downloadImage(
-                "nums",
-                (gotten) -> assertArrayEquals(gotten, new byte[]{2, 3, 4}),
-                (exc) -> {
-                }, getApplicationContext()
-        );
-        us.deleteImage(
-                "nums",
-                (name) -> assertEquals(name, "nums"),
-                (exc) -> {
-                }, getApplicationContext()
-        );
+
+
     }
 }
